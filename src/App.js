a74d33ee9c929a3cb0128f14dca7ec067754d556
xa-fz <553866469@qq.com>
2021-08-22 17:58:23 +0800

    配置antd-mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd-mobile';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button type="primary">primary</Button>
     </div>
   );
 }
