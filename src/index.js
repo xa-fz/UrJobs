311b108744e9bf3ae442d106f539b521a6a05626
xa-fz <553866469@qq.com>
2021-08-22 18:26:41 +0800

    实现组件样式按需加载

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
-import 'antd-mobile/dist/antd-mobile.css'
 
 ReactDOM.render(
   <React.StrictMode>
