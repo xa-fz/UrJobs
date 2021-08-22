01cca9ca2565f513e98a397c00a52c113a9a1147
xa-fz <553866469@qq.com>
2021-08-22 18:20:37 +0800

    配置组件库

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
+import 'antd-mobile/dist/antd-mobile.css'
 
 ReactDOM.render(
   <React.StrictMode>
