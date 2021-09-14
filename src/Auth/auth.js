d67262682f9d0ae0eed5d32335533a10940587b1
xa-fz <553866469@qq.com>
2021-09-15 00:44:55 +0800

    添加axios以及和redux联合使用

--- src/Auth/auth.js
+++ src/Auth/auth.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { login } from './redux';
+import { login, getUser } from './redux';
 import { Button } from 'antd-mobile';
 import { Redirect } from 'react-router-dom';
 
 class Auth extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           
+        }
+    }
+
+    componentDidMount () {
+        this.props.getUser();
+    }
     render () {
+        const { user, age } = this.props;
         return (
             <div>
+                <h2>我的名字：{user}, 年龄是：{age}</h2>
                 {
                     this.props.isAuth ? <Redirect to="/dashboard"/> :
                         <Button onClick={() => {
@@ -22,11 +34,14 @@ class Auth extends React.Component {
 const mapStatetoProps = (state) => {
     const { auth } = state;
     return {
-        isAuth: auth.isAuth
+        isAuth: auth.isAuth,
+        user: auth.user,
+        age: auth.age
     }
 }
 // 方法
 const actionCreators = {
-    login
+    login,
+    getUser
 }
 export default connect(mapStatetoProps, actionCreators)(Auth);
--- src/Auth/redux.js
+++ src/Auth/redux.js
@@ -1,16 +1,29 @@
-const LOGIN = 'LOGIN';
-const LOGOUT = 'LOGOUT';
+import axios from 'axios';
 
-function auth (state = {isAuth: false, name: 'admin'}, action) {
+const initData = {
+    isAuth: false,
+    user: '',
+    age: 0
+}
+
+function auth (state = initData, action) {
+    console.log(state, action);
     switch (action.type) {
         case 'LOGIN':
             return {...state, isAuth: true}
         case 'LOGOUT':
             return {...state, isAuth: false}
+            case 'USER_INFO':
+            return {...state, ...action.payload}
         default: 
         return state
     }
 }
+
+// 声明action
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+const USER_INFO = 'USER_INFO';
 
 export function login () {
     return {
@@ -21,6 +34,23 @@ export function login () {
 export function logout () {
     return {
         type: LOGOUT
+    }
+}
+
+export function userdata (data) {
+    return {
+        type: USER_INFO,
+        payload: data
+    }
+}
+
+// 调用接口
+export function getUser () {
+    return dispatch => {
+        axios.get('/data')
+            .then(res => {
+                dispatch(userdata(res.data[0]))
+            })
     }
 }
 
